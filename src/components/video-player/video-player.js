1f820e31c072f8cdc978e435601ccadbd8bd0a07
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-10-16 17:57:28 +0100

    Update video-player.js

--- src/components/video-player/video-player.js
+++ src/components/video-player/video-player.js
@@ -1,17 +1,23 @@
-import styled from '@emotion/styled/macro';
-import React from 'react';
-import ReactPlayer from 'react-player'
+import styled from "@emotion/styled/macro"
+import React from "react"
+import ReactPlayer from "react-player"
 
 const VideoPlayerWrapper = styled.div`
-    width:100%;
+  width: 100%;
+  position: relative;
+  padding-top: 56.25%; /* Player ratio: 100 / (1280 / 720) */
+
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 `
 // Render a YouTube video player
-export const  VideoPlayer = ({url}) => {
-    return (
-        <VideoPlayerWrapper>
-        <ReactPlayer width={'100%'} url={url} />
-
-        </VideoPlayerWrapper>
-    );
+export const VideoPlayer = ({ url }) => {
+  return (
+    <VideoPlayerWrapper>
+      <ReactPlayer className='react-player' controls={true} width={"100%"} height={"100%"} url={url} />
+    </VideoPlayerWrapper>
+  )
 }
-
